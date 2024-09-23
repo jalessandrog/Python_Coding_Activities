f7811735e7a6578c7783ec3d093e9c342dda9632
Joseph Garcia <jalessandro.ag@gmail.com>
2024-09-22 18:26:34 -0600

    First activity - Cipher

---
+++ Beginner/Cipher.py
@@ -0,0 +1,14 @@
+text = 'Hello Zaira'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = index + shift
+        encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+    
