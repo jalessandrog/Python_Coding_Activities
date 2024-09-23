6c93dca9b42781d19d7009f93111c05bcb19aeec
Joseph Garcia <jalessandro.ag@gmail.com>
2024-09-22 19:00:22 -0600

    Caesar Chipher Activitiy

--- Beginner/Cipher.py
+++ Beginner/Cipher.py
@@ -1,14 +1,34 @@
-message = 'Hello Joseph'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789' 
-encrypted_message = ''
+# Notes
+'''
+Variables defined outside a function have a global scope and they can be accessed from any part of your code.
+Variables defined inside a function are local to that function and cannot be accessed outside of it.
+'''
 
-for word in message.lower():
-    if word == ' ':
-        encrypted_message += word
-    else:
-        index = alphabet.find(word)
-        # To avoid an error in script when the loop reaches the end of the  alphabet len variable and sum index + shift exceeds that 
-        new_index = (index + shift) % len(alphabet)
-        encrypted_message += alphabet[new_index]
-    print('word:', word, 'encrypted message:', encrypted_message)
+# This function receive two arguments: the non-encrypted message and the offset
+def Caesar_Cipher(plain_message, offset): 
+    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789' 
+    encrypted_message = ''
+
+    for word in plain_message.lower():
+        if word == ' ':
+            encrypted_message += word
+        else:
+            index = alphabet.find(word)
+            # To avoid an error in script when the loop reaches the end of the  alphabet len variable and sum index + shift exceeds that 
+            new_index = (index + offset) % len(alphabet)
+            encrypted_message += alphabet[new_index]
+    
+    print('plain message:', plain_message)
+    print('encrypted message:', encrypted_message)
+
+# Examples
+message = 'Hello Joseph'
+
+# Example 1 with offset as 3
+Caesar_Cipher(message, 3)
+
+# Example 2 with offset as 10
+Caesar_Cipher(message, 10)
+
+# Example 2 with offset as 13
+Caesar_Cipher(message, 13)
